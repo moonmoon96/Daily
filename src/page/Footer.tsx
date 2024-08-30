9b67000cf73c02b2586ac061f0580188aa139f7e
JunHyeon <whiteswolve@naver.com>
2024-08-30 16:43:56 +0900

    footer 추가

---
+++ src/page/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer () {
+    return(
+        <>
+            <footer className="footer">
+                <div className="footer-content">
+                    <div className="footer-content-top">
+                        <div className="footer-content-top wrap">
+                            <div>개인정보 취급방침 |&nbsp;</div>
+                            <div>문의</div>
+                        </div>
+                    </div>
+                    <div className="footer-content-bottom">
+                        © 2024 daily All rights reserved. This site is not associated with NEXON Korea. Data sourced from NEXON OpenAPI.
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
--- src/page/home.tsx
+++ src/page/home.tsx
@@ -5,6 +5,7 @@ import { fetchOcid } from "../store/OcidSlice";
 import { fetchBasicData } from "../store/BasicSlice";
 import { BasicData } from "../api/character/Types";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function Home () {
 
@@ -33,30 +34,35 @@ function Home () {
 
     return (
         <>
-            <Header />
-            <input 
-                placeholder="캐릭터명을 입력하세요"
-                value={characterName}
-                onChange={(e) => setCharacterName(e.target.value)}
-            />
-            <button onClick={clickOcid}>
-                클릭
-            </button>
-            <div>
-                {basicStatus === 'loading' && <p>로딩 중...</p>}
-                {basicStatus === 'failed' && <p>데이터를 가져오는 데 실패했습니다.</p>}
-                {basicStatus === 'succeeded' && basicData && (
+            <div className="daily">
+                <Header />
+                <div className="content">
+                    <input 
+                        placeholder="캐릭터명을 입력하세요"
+                        value={characterName}
+                        onChange={(e) => setCharacterName(e.target.value)}
+                    />
+                    <button onClick={clickOcid}>
+                        클릭
+                    </button>
                     <div>
-                        <h2>캐릭터 정보</h2>
-                        <p>이름: {basicData.character_name}</p>
-                        <p>서버: {basicData.world_name}</p>
-                        <p>성별: {basicData.character_gender}</p>
-                        <p>직업: {basicData.character_class}</p>
-                        <p>레벨: {basicData.character_level}</p>
-                        <p>경험치: {basicData.character_exp_rate}</p>
-                        <img src={basicData.character_image} alt={basicData.character_name} />
+                        {basicStatus === 'loading' && <p>로딩 중...</p>}
+                        {basicStatus === 'failed' && <p>데이터를 가져오는 데 실패했습니다.</p>}
+                        {basicStatus === 'succeeded' && basicData && (
+                            <div>
+                                <h2>캐릭터 정보</h2>
+                                <p>이름: {basicData.character_name}</p>
+                                <p>서버: {basicData.world_name}</p>
+                                <p>성별: {basicData.character_gender}</p>
+                                <p>직업: {basicData.character_class}</p>
+                                <p>레벨: {basicData.character_level}</p>
+                                <p>경험치: {basicData.character_exp_rate}</p>
+                                <img src={basicData.character_image} alt={basicData.character_name} />
+                            </div>
+                        )}
                     </div>
-                )}
+                </div>
+                <Footer />
             </div>
         </>
     );
