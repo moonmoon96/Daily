901be081f9d9b55172520fc27a9d0306421b841a
JunHyeon <whiteswolve@naver.com>
2024-08-30 14:20:49 +0900

    캐릭터 ocid 발급 api

--- src/api/character/ocid.ts
+++ src/api/character/ocid.ts
@@ -1,10 +1,9 @@
-const characterName = "준현";
-const urlString = "https://open.api.nexon.com/maplestory/v1/character/basic?ocid=a29a4a917f119c67cc89cd557eb32e45efe8d04e6d233bd35cf2fabdeb93fb0d"
-
-export async function ocid() {
+export async function ocid(characterName : string) {
     try {
         const apiKey = process.env.REACT_APP_API_KEY;
-        
+        const urlString = "https://open.api.nexon.com/maplestory/v1/id?character_name=" + characterName;
+
+
         if (!apiKey) {
             throw new Error('API key is missing');
         }
@@ -14,8 +13,13 @@ export async function ocid() {
                 "x-nxopen-api-key": apiKey
             }
         });
+        
         const data = await response.json();
-        console.log(data);
+        const ocid = data.ocid;
+        console.log(ocid);
+
+        return ocid;
+        
     } catch (error) {
         console.error(error);
     }
--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,18 +1,52 @@
 import { useState } from "react";
-import { ocid } from "../api/ocid"
+import { ocid } from "../api/character/ocid"
+import { basic } from "../api/character/basic";
+import { BasicData } from "../api/character/types";
 
 function Home () {
 
     const [characterName, setCharacterName] = useState("");
+    const [basicData, setBasicData] = useState<BasicData | null>(null);
+
+    const clickOcid = async() => {
+        if(characterName) {
+            try{
+                const ocidValue = await ocid(characterName);
+                if(ocidValue) {
+                    const data = await basic(ocidValue);
+                    setBasicData(data);
+                }
+            }
+            catch(error) {
+                console.log(error);
+            }
+        }
+    }
 
     return(
         <>
             <input 
                 placeholder="캐릭터명을 입력하세요"
+                value={characterName}
+                onChange={(e) => setCharacterName(e.target.value)}
             />
-            <button onClick={ ocid }>
+            <button onClick={clickOcid}>
                 클릭
             </button>
+            <div>
+                {basicData && (
+                        <div>
+                            <h2>캐릭터 정보</h2>
+                            <p>이름: {basicData.character_name}</p>
+                            <p>서버: {basicData.world_name}</p>
+                            <p>성별: {basicData.character_gender}</p>
+                            <p>직업: {basicData.character_class}</p>
+                            <p>레벨: {basicData.character_level}</p>
+                            <p>경험치: {basicData.character_exp_rate}</p>
+                            <img src={basicData.character_image} alt={basicData.character_name} />
+                        </div>
+                    )}
+            </div>
         </>
     )
 }
