edfefc46fb968e1c1dbcc3e9472f077b41c6e78b
JunHyeon <whiteswolve@naver.com>
2024-09-07 23:47:15 +0900

    이미지 및 갱신날짜 개요 추가

--- src/page/character/Char.tsx
+++ src/page/character/Char.tsx
@@ -52,7 +52,22 @@ export default function char () {
                                             <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24">
                                                 <path d="m22 9.24-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z" />
                                             </svg>
-                                        </button>
+                                            </button>
+                                        </div>
+                                        <div className="char-top charspec">
+                                            <p className="char-top firstp">
+                                                <span>길드</span>
+                                                <a></a>
+                                            </p>
+                                            <p className="char-top p">
+                                                <span>인기도</span>
+                                            </p>
+                                            <p className="char-top p">
+                                                <span>종합랭킹</span>
+                                            </p>
+                                            <p className="char-top p">
+                                                <span>직업랭킹</span>
+                                            </p>
                                         </div>
                                     </div>
                                     <div className="char-top level">
