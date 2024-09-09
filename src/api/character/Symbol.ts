992db05a73a045b817ea90329a224282e227b426
JunHyeon <whiteswolve@naver.com>
2024-09-09 23:32:36 +0900

    장착 심볼 api

---
+++ src/api/character/Symbol.ts
@@ -0,0 +1,23 @@
+export async function item(ocid : string) {
+    try {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        const urlString = "https://open.api.nexon.com/maplestory/v1/character/symbol-equipment?ocid=" + ocid;
+
+
+        if (!apiKey) {
+            throw new Error('API key is missing');
+        }
+
+        const response = await fetch(urlString, {
+            headers: {
+                "x-nxopen-api-key": apiKey
+            }
+        });
+        const symbol_data = await response.json();
+
+        return symbol_data;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
