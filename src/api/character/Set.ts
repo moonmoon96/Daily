82b5d0e0e9078919ec049ee1e607e2b82c61c7a7
JunHyeon <whiteswolve@naver.com>
2024-09-09 23:33:56 +0900

    세트 효과 api

---
+++ src/api/character/Set.ts
@@ -0,0 +1,23 @@
+export async function item(ocid : string) {
+    try {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        const urlString = "https://open.api.nexon.com/maplestory/v1/character/set-effect?ocid=" + ocid;
+
+
+        if (!apiKey) {
+            throw new Error('API key is missing');
+        }
+
+        const response = await fetch(urlString, {
+            headers: {
+                "x-nxopen-api-key": apiKey
+            }
+        });
+        const set_data = await response.json();
+
+        return set_data;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
