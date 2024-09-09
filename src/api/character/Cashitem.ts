c5c257c1146069f9533ad7585cb4a8d47e777c05
JunHyeon <whiteswolve@naver.com>
2024-09-09 23:31:10 +0900

    캐시장비 api

---
+++ src/api/character/Cashitem.ts
@@ -0,0 +1,23 @@
+export async function cashitem(ocid : string) {
+    try {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        const urlString = "https://open.api.nexon.com/maplestory/v1/character/cashitem-equipment?ocid=" + ocid;
+
+
+        if (!apiKey) {
+            throw new Error('API key is missing');
+        }
+
+        const response = await fetch(urlString, {
+            headers: {
+                "x-nxopen-api-key": apiKey
+            }
+        });
+        const cashitem_data = await response.json();
+
+        return cashitem_data;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
