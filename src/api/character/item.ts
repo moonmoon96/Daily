bf4c3b6ec407d71d6bd8d99bd60d81b9e67b0ce4
JunHyeon <whiteswolve@naver.com>
2024-09-09 23:29:59 +0900

    장착 장비 api

---
+++ src/api/character/item.ts
@@ -0,0 +1,23 @@
+export async function item(ocid : string) {
+    try {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        const urlString = "https://open.api.nexon.com/maplestory/v1/character/item-equipment?ocid=" + ocid;
+
+
+        if (!apiKey) {
+            throw new Error('API key is missing');
+        }
+
+        const response = await fetch(urlString, {
+            headers: {
+                "x-nxopen-api-key": apiKey
+            }
+        });
+        const item_data = await response.json();
+
+        return item_data;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
--- src/page/character/Char.tsx
+++ src/page/character/Char.tsx
@@ -25,7 +25,7 @@ export default function char () {
                                         </div>
                                     </div>
                                     <div className="char-top aximgdate">
-                                        <button></button>
+                                        <button>날짜</button>
                                         <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24">
                                             <path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z" />
                                         </svg>
@@ -71,7 +71,18 @@ export default function char () {
                                         </div>
                                     </div>
                                     <div className="char-top level">
-
+                                        <div>
+                                            <p>레벨</p>
+                                            <h5></h5>
+                                        </div>
+                                        <div>
+                                            <p>유니온</p>
+                                            <h5></h5>
+                                        </div>
+                                        <div>
+                                            <p>전투력</p>
+                                            <h5></h5>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
