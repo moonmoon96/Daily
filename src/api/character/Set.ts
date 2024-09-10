f994771a26e4b32fa35da1b71b10c578f8bca0c0
JunHyeon <whiteswolve@naver.com>
2024-09-10 23:48:56 +0900

    세트 효과 api 수정

--- src/api/character/Set.ts
+++ src/api/character/Set.ts
@@ -1,4 +1,4 @@
-export async function item(ocid : string) {
+export async function set(ocid : string) {
     try {
         const apiKey = process.env.REACT_APP_API_KEY;
         const urlString = "https://open.api.nexon.com/maplestory/v1/character/set-effect?ocid=" + ocid;
